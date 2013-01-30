8d54ad297d73d221ad455a3a99825b853a067eea
Sebastian <sebastian.campbell@sydney.edu.au>
2013-01-30 18:11:58 +1100

    first commit

---
+++ run_slidify.R
@@ -0,0 +1,10 @@
+##Get slidify
+# require(devtools)
+## dev_mode()  #Turn on devmode if you don't want to affect your library
+# install_github("slidify", "ramnathv")
+# install_github("slidifyLibraries", "ramnathv")
+# install_github("knitcitations", "cboettig")
+
+library(slidify)
+
+slidify("index.Rmd")
